f8dd4fb57c3484ed8e6d0baf40cfa66b754e90f8
Vasyl <vasyl.shumskyi@gmail.com>
2017-04-03 10:54:08 +0300

    007 Managing window events

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const { BrowserWindow, app } = electron
 app.on('ready', function() {
   let appWindow, infoWindow
 
-  appWindow = new BrowserWindow()
+
+  appWindow = new BrowserWindow({
+    show: false
+  })
   appWindow.loadURL('https://electron.atom.io')
 
   infoWindow = new BrowserWindow({
     width: 400,
     height: 300,
     transparent: true,
+    show: false,
     frame: false
   })
 
   infoWindow.loadURL('file://' + __dirname + '/info.html')
+
+  appWindow.once('ready-to-show', function() {
+    appWindow.show()
+    // will show the popup after 1 second and hide it after 3 seconds
+    setTimeout(function () {
+      infoWindow.show()
+      setTimeout(function () { infoWindow.hide() }, 3000);
+    }, 1000);
+  })
 })
